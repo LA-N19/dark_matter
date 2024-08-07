f7049a64c0c19b42b3d6507e07b177164515c612
LA-N19 <154728717+LA-N19@users.noreply.github.com>
2024-08-07 07:10:02 +0200

    Update n19_drum_pats.rb

--- n19_drum_pats.rb
+++ n19_drum_pats.rb
@@ -1,11 +1,29 @@
 """
-n19_drum_pats.rb - 200 beats 100 lines of code
+n19_drum_pats.rb - read and play 200 beats in 100 lines of code (sonic pi)
 
-20240802 - L.A.N19 - in D:\LA-N19\dark_matter / https://github.com/LA-N19
-  reading n19_drum_pats.txt and play all the beats 
-    https://github.com/montoyamoraga/drum-machine-patterns/readme-ov-file
+reading the beats from [infile_md] and play all of those 200 beats 4 times
+
+History:
+20240802 - L.A.N19 - Creation in D:\LA-N19\dark_matter / https://github.com/LA-N19
+20240806 - L.A.N19 - version 0.1
+
+Usage: 
+   1) open this file in sonic pi 
+   2) copy https://github.com/montoyamoraga/drum-machine-patterns/README.md 
+        and the set infile_md to this file
+   3) and 'play'!
+
+Next:
+  a) Midi-support to choose beat and sample set on the run
+  b) implement Accent to substitue the blib
+  c) add fx (compression, reverb etc.pp.)
+  d) add beat generator (random lane; crossing? bit like night train)
+  e) remember Funk 15: this fat! R&B 3 is good. 4 also? whatever; disco 2 ride the horse!
+  f) read beat and bpm and so on from md file
+  g) load & save and so on
+  h) new video only with pattern and needle ()
 """
-path = 'D:\LA-N19\dark_matter\\'
+infile_md, outfile_json  = 'D:\LA-N19\dark_matter\n19_drum_pats.md', nil
 n19_drum_samples = { BD: :drum_bass_hard,
                      SD: :drum_snare_hard,
                      LT: :drum_tom_lo_hard,
@@ -15,30 +33,10 @@ n19_drum_samples = { BD: :drum_bass_hard,
                      OH: :drum_cymbal_open,
                      CY: :drum_cymbal_soft,
                      RS: :elec_tick, # usually rimshot..
-                     CP: :elec_plip, # usually clap..
+                     CP: :perc_snap, # snap: best i could find in the common sonic pi samples...
                      CB: :drum_cowbell,
                      AC: :elec_blip, # later on we'll use other samples or compression-fx for Accent...
                      }
-
-"""
-Rock 1 Measure A
-4/4, quarter note 112-139
-"""
-
-def pad_2_array(pat, n19_drum_samples, column_sep="\t")
-  pat_to_at = {}
-  pat.split("\n").each_with_index { |p,pi|
-    pa = p.split(column_sep)
-    next if pa.length < 2
-    s = pa.shift.to_sym
-    if not n19_drum_samples.keys.include?(s) then
-      print("pad_2_array: could'nt find", s)
-      next
-    end
-    pat_to_at[s] = pa
-  }
-  return pat_to_at
-end
 
 def play_pat(pat, n19_drum_samples, debug=true, bpm=130)
   bpm = rrand_i(*pat[:bpm]) if bpm == true
@@ -56,15 +54,33 @@ def play_pat(pat, n19_drum_samples, debug=true, bpm=130)
 end
 
 def import_pads(ls)
+  "
+    reads the pattern from the .md file as give by https://github.com/montoyamoraga/drum-machine-patterns/README.md 
+
+    parameter:
+      the md-file as array of strings
+
+    return (return with one example entry):
+       { 'rock 1 - Measure A': 
+           { name: 'rock 1 - Measure A', category: 'rock 1', part: 'Measure A',
+             beats: 4, note_length: 4, bpm: [112, 139], # just default in the moment...
+             pattern: {
+               ch: ['X','','X','X','X','','X','X','X','','X','X','X','','X','X']
+             }
+           } 
+      }
+    oh man! just see n19_drum_pats.json!
+  "
   start, tpats = false, {}
+  # TODO: basic beat informations are not grabbed yet...
   pat_act = { pattern: {}, beats: 4, note_length: 4, bpm: [112, 139], }
   cat, prt, pats = nil
   ls.each { |l|
     start = true if l.strip == "## Patterns from the book *200 Drum machine patterns*"
     next if l.length < 6 or not start
     typ = nil
-    typ, cat = "cat", l[4..-1].strip if l[0,4] == "### "
-    typ, prt = "prt", l[5..-1].strip if l[0,5] == "#### "
+    typ, cat = "cat", l[4..-1].strip if l[0,4] == "### " # category
+    typ, prt = "prt", l[5..-1].strip if l[0,5] == "#### "  # part
     typ = "pat" if l[0] == "|" and l[0..4] != "|----" and l[0..4] != "|Drum"
     pat_act[:pattern] = {} if typ == "cat" or typ == "prt"
     next if not typ == "pat"
@@ -77,8 +93,10 @@ def import_pads(ls)
   }
   return tpats
 end
-pates = import_pads(File.readlines(path+'n19_drum_pats.txt'))
-File.write(path+'n19_drum_pats.json', JSON.dump(pates))
+
+pates = import_pads(File.readlines(infile_md))
+File.write(outfile_json, JSON.dump(pates)) if outfile_json
+
 live_loop :drum do
   i = 0
   pates.each { |nam,pat|
@@ -86,3 +104,13 @@ live_loop :drum do
     4.times { |t| sleep play_pat(pat, n19_drum_samples, debug=t==0) } # that a sleep in an at nested works!!!
   }
 end
+"""
+  somes lines left..., so i could have documented more
+  somes lines left..., so i could have coded not that compressed ;)
+  somes lines left..., so i could have coded to read beat and bpm and so on
+  somes lines left...
+  somes lines left...
+  somes lines left...
+  somes lines left...
+  somes lines left...
+"""
